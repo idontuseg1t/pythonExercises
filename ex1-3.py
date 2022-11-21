a96cce7dc40a674964064d0885892655806075db
idontuseg1t <111327977+idontuseg1t@users.noreply.github.com>
2022-11-21 02:05:42 +0000

    Create ex1-3.py

---
+++ ex1-3.py
@@ -0,0 +1,14 @@
+# input
+
+temp_C = float(input("Enter degrees in celsius: ")) 
+# the float() converts the input to a number
+
+# Process
+
+temp_F = (temp_C * 1.8) + 32
+
+# Output
+
+# Hint: Rounding
+
+print(f"The temperature in farenheit is {round(temp_F,1)}")
