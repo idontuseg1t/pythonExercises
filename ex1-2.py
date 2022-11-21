84e05164e19e34cba2bb95955df8c9f4bd28cbc0
idontuseg1t <111327977+idontuseg1t@users.noreply.github.com>
2022-11-21 02:04:54 +0000

    Create ex1-2.py

---
+++ ex1-2.py
@@ -0,0 +1,12 @@
+# Input age  
+age = int(input("Enter your age in years: "))  # the int() function converts the input to an int  
+  
+
+# Process
+
+# dog_age = age * 7*    # FIX THIS
+
+  
+# Output dog age
+
+print(f"Your age in dog years is {age * 7} ")       # FIX THIS
