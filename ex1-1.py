a35bc15124ade1c05d5511802a84285706ffd52e
idontuseg1t <111327977+idontuseg1t@users.noreply.github.com>
2022-11-21 02:04:11 +0000

    Create ex1-1.py

---
+++ ex1-1.py
@@ -0,0 +1,9 @@
+# Input names  
+first_name = input("First name: ")  
+Last_name = input("Last name: ")  
+# Process names to produce output (optional)  
+  
+# Print output  
+print("The name's " + first_name + “ ” +  last_name)
+# OR
+print(f"The name's {first_name} {last_name}")
